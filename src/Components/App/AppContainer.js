d7d975e884efee04165e3aa61de693440716c9aa
limgeun <shing100@naver.com>
2019-04-26 15:31:21 +0900

    설치

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <code>class hello world</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
