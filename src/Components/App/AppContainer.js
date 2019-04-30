43e6187e733b08256b43cb7a6962dadeb0324e19
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-04-30 17:39:57 +0900

    Routes

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { ThemeProvider } from "styled-components"
 import GlobalStyles from '../../Styles/GlobalStyles';
 import Theme from '../../Styles/Theme';
+import AppRouter from '../Router';
 
 const AppContainer = () => (
   <ThemeProvider theme={Theme}>
+  <>
     <GlobalStyles />
+    <AppRouter isLoggedIn={true} />
+  </>
   </ThemeProvider>
 );
 
---
+++ src/Components/Router.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../Routes/Auth";
+import Feed from "../Routes/Feed";
+
+const LoggedInRoutes = () => (
+    <>
+        <Route exact path="/" component={Feed} />
+    </>
+)
+
+const LoggedOutRoutes = () => (
+    <>
+        <Route exact path="/" component={Auth} />
+    </>
+)
+
+const AppRouter = ({ isLoggedIn }) => (
+    <Router>
+        <Switch>
+            {isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}
+        </Switch>
+    </Router>
+)
+
+// eslint-disable-next-line react/no-typos
+AppRouter.PropTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default AppRouter;
