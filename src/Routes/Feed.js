3062aab089fde606bad04fd926616313ed64d321
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-05-13 20:14:18 +0900

    Feed Route 와 Loader Components

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,57 @@
-export default () => "Feed";
+import React from "react";
+import styled from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+
+const FEED_QUERY = gql`
+{
+    seeFeed {
+        ok
+        posts {
+            id
+            location
+            caption
+            user {
+                id
+                avatar
+                username
+            }
+            files {
+                id
+                url
+            }
+            likeCount
+            isLiked
+            comments {
+                id
+                text
+                user {
+                    id
+                    username
+                    createdAt
+                }
+            }
+        }
+    }
+}
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 60vh
+`;
+
+export default () => {
+    const { data, loading } = useQuery(FEED_QUERY);
+    if(loading){
+        return (
+            <Wrapper>
+                {loading && <Loader />}
+            </Wrapper>
+        )
+    }else {
+        return null;
+    }
+}
