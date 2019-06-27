6e05b3e07b12bfaf679e62be4b0cac5c9644df69
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-06-27 15:53:46 +0900

    FollowButton 1

---
+++ src/Components/FollowButton/FollowButtonContainer.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useMutation } from "react-apollo-hooks";
+import { FOLLOW, UNFOLLOW } from "./FollowButtonQueries";
+import FollowButtonPresenter from "./FollowButtonPresenter";
